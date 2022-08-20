38207c039a5a15d60b96e30992f583818257f383
DannyDanBull123 <110248018+DannyDanBull123@users.noreply.github.com>
2022-08-20 20:37:16 +0530

    Update grammarly.py

--- grammarly.py
+++ grammarly.py
@@ -1,8 +1,16 @@
 #import textblob
 from textblob import TextBlob
+while True:
+  #variable
+    a = TextBlob(input("Please enter your sentence here: "))
+    a = a.correct()
+    print(a)
+
+    b = input("Press Y for if it was correct and N if the correction was wrong : ")
+    if b == "y":
+        print("I was right!")
+        quit()
+    elif b == "n":
+        c=input("Please enter your phrase again: ")
 
 #variable
-a = TextBlob(input("Please enter your word here : "))
-a = a.correct()
-
-print(a)
